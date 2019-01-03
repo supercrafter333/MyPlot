c9ec91ae88e0dbeb1250921f0507c9f7d0a065bf
jasonwynn10 <jasonwynn10@me.com>
2019-01-03 13:33:28 -0500

    Added API to tell if a plot is merged Plots can only be merged once at a time, not by two different owners
    Cannot use an aabb variable or merge counter in the plot object because of changeability and public access

--- src/MyPlot/MyPlot.php
+++ src/MyPlot/MyPlot.php
@@ -284,9 +284,6 @@ class MyPlot extends PluginBase
 		$plotSize = $plotLevel->plotSize;
 		$roadWidth = $plotLevel->roadWidth;
 		$totalSize = $plotSize + $roadWidth;
-		for($i = 0; $i < $plot->merged; $i++) {
-			$totalSize += $plotSize + $roadWidth;
-		}
 		$x = $totalSize * $plot->X;
 		$z = $totalSize * $plot->Z;
 		$level = $this->getServer()->getLevelByName($plot->levelName);
@@ -483,6 +480,16 @@ class MyPlot extends PluginBase
 	public function unMergePlots(Plot $plotA, Plot $plotB) : bool {
 		// TODO: logic needed to decide how plots are connected and how to deal with excluded plots inside
 		return false; // failure
+	}
+
+	/**
+	 * @param Plot $plot
+	 *
+	 * @return bool
+	 */
+	public function isPlotMerged(Plot $plot) : bool {
+		$base = $this->dataProvider->getMergedBase($plot);
+		return $plot !== $base;
 	}
 
 	/**
