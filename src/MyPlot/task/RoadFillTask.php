fadfee9f4f07047e36566c3a43b4d644e2e7dcd6
jasonwynn10 <jasonwynn10@me.com>
2020-05-17 17:03:01 -0400

    Completed Road fill task

--- src/MyPlot/task/RoadFillTask.php
+++ src/MyPlot/task/RoadFillTask.php
@@ -11,70 +11,114 @@ use pocketmine\scheduler\Task;
 
 class RoadFillTask extends Task {
 	/** @var MyPlot $plugin */
-	private $plugin;
-	private $roadCounts, $plots, $level, $height, $bottomBlock, $plotFillBlock, $plotFloorBlock, $plotBeginPos, $xMax, $zMax, $maxBlocksPerTick;
+	protected $plugin;
+	/** @var Plot $plot */
+	protected $plot;
+	/** @var \pocketmine\level\Level|null $level */
+	protected $level;
+	/** @var int $height */
+	protected $height;
+	/** @var Block $plotWallBlock */
+	protected $plotWallBlock;
+	/** @var Position|Vector3|null $plotBeginPos */
+	protected $plotBeginPos;
+	/** @var int $xMax */
+	protected $xMax;
+	/** @var int $zMax */
+	protected $zMax;
+	/** @var Block $roadBlock */
+	protected $roadBlock;
+	/** @var Block $groundBlock */
+	protected $groundBlock;
+	/** @var Block $bottomBlock */
+	protected $bottomBlock;
+	/** @var int $maxBlocksPerTick */
+	protected $maxBlocksPerTick;
+	/** @var Vector3 $pos */
+	protected $pos;
+
+	//private $roadCounts, $plots, $level, $height, $bottomBlock, $plotFillBlock, $plotFloorBlock, $plotBeginPos, $xMax, $zMax, $maxBlocksPerTick;
 
 	/**
 	 * PlotMergeTask constructor.
 	 *
 	 * @param MyPlot $plugin
-	 * @param Plot[] $plots
+	 * @param Plot $start
+	 * @param Plot $end
 	 * @param int $maxBlocksPerTick
 	 */
-	public function __construct(MyPlot $plugin, array $plots, int $maxBlocksPerTick = 256) {
-		$this->plugin = $plugin;
-		foreach($plots as $plot) {
-			$key = $plot->__toString().$plot->levelName;
-			$this->roadCounts[$key] = count($plugin->getProvider()->getMergedPlots($plot, true));
-			$this->plots[$key] = $plot;
+	public function __construct(MyPlot $plugin, Plot $start, Plot $end, int $maxBlocksPerTick = 256) {
+		if($start->isSame($end) or !(abs($start->X - $end->X) < 2 or abs($start->Z - $end->Z) < 2)) {
+			throw new \Exception("Invalid Plot arguments");
 		}
+
+		$this->plugin = $plugin;
+		$this->plot = $start;
+		$this->plotBeginPos = $plugin->getPlotPosition($start);
+		$this->level = $this->plotBeginPos->getLevel();
+
+		$plotLevel = $plugin->getLevelSettings($start->levelName);
+		$plotSize = $plotLevel->plotSize;
+		$roadWidth = $plotLevel->roadWidth;
+
+		if(($start->X - $end->X) === 1) {
+			$this->plotBeginPos = $this->plotBeginPos->add(0,0,$plotSize);
+			$this->xMax = (int)($this->plotBeginPos->x + $plotSize);
+			$this->zMax = (int)($this->plotBeginPos->z + $roadWidth);
+		}elseif(($start->X - $end->X) === -1) {
+			$this->plotBeginPos = $this->plotBeginPos->subtract(0,0,$roadWidth);
+			$this->xMax = (int)($this->plotBeginPos->x + $plotSize);
+			$this->zMax = (int)($this->plotBeginPos->z + $roadWidth);
+		}elseif(($start->Z - $end->Z) === 1) {
+			$this->plotBeginPos = $this->plotBeginPos->subtract($roadWidth);
+			$this->xMax = (int)($this->plotBeginPos->x + $roadWidth);
+			$this->zMax = (int)($this->plotBeginPos->z + $plotSize);
+		}elseif(($start->Z - $end->Z) === -1) {
+			$this->plotBeginPos = $this->plotBeginPos->add($plotSize);
+			$this->xMax = (int)($this->plotBeginPos->x + $roadWidth);
+			$this->zMax = (int)($this->plotBeginPos->z + $plotSize);
+		}
+
+		$this->height = $plotLevel->groundHeight;
+		//$this->plotWallBlock = $plotLevel->wallBlock; TODO: delete?
+		$this->roadBlock = $plotLevel->plotFloorBlock;
+		$this->groundBlock = $plotLevel->plotFillBlock;
+		$this->bottomBlock = $plotLevel->bottomBlock;
+
 		$this->maxBlocksPerTick = $maxBlocksPerTick;
+		$this->pos = new Vector3($this->plotBeginPos->x, 0, $this->plotBeginPos->z);
+
+		$plugin->getLogger()->debug("Road Clear Task started between plots {$start->X};{$start->Z} and {$end->X};{$end->Z}");
 	}
 
 	public function onRun(int $currentTick) {
-		foreach($this->roadCounts as $key => $count) {
-			$plot = $this->plots[$key];
-			$plotBeginPos = $this->plugin->getPlotPosition($plot);
-			$level = $plotBeginPos->getLevel();
-			$plotLevel = $this->plugin->getLevelSettings($plot->levelName);
-			$plotSize = $plotLevel->plotSize;
-			$xMax = $plotBeginPos->x + $plotSize;
-			$zMax = $plotBeginPos->z + $plotSize;
-			$height = $plotLevel->groundHeight;
-			$bottomBlock = $plotLevel->bottomBlock;
-			$plotFillBlock = $plotLevel->plotFillBlock;
-			$plotFloorBlock = $plotLevel->plotFloorBlock;
-			$blocks = 0;
-			while($pos->x < $xMax) {
-				while($pos->z < $zMax) {
-					if($this->plugin->getPlotByPosition(Position::fromObject($pos, $level)) === null) {
-						while($pos->y < $level->getWorldHeight()) {
-							if($pos->y === 0) {
-								$block = $bottomBlock;
-							}elseif($pos->y < $height) {
-								$block = $plotFillBlock;
-							}elseif($pos->y === $height) {
-								$block = $plotFloorBlock;
-							}else{
-								$block = Block::get(Block::AIR);
-							}
-							$level->setBlock($pos, $block, false, false);
-							$blocks++;
-							if($blocks >= $this->maxBlocksPerTick) {
-								$this->plugin->getScheduler()->scheduleDelayedTask($this, 1);
-								return;
-							}
-							$pos->y++;
-						}
-						$pos->y = 0;
+		$blocks = 0;
+		while($this->pos->x < $this->xMax) {
+			while($this->pos->z < $this->zMax) {
+				while($this->pos->y < $this->level->getWorldHeight()) {
+					if($this->pos->y === 0) {
+						$block = $this->bottomBlock;
+					}elseif($this->pos->y < $this->height) {
+						$block = $this->groundBlock;
+					}elseif($this->pos->y === $this->height) {
+						$block = $this->roadBlock;
+					}else{
+						$block = Block::get(Block::AIR);
 					}
-					$pos->z++;
+					$this->level->setBlock($this->pos, $block, false, false);
+					$blocks++;
+					if($blocks >= $this->maxBlocksPerTick) {
+						$this->plugin->getScheduler()->scheduleDelayedTask($this, 1);
+						return;
+					}
+					$this->pos->y++;
 				}
-				$pos->z = $plotBeginPos->z;
-				$pos->x++;
+				$this->pos->y = 0;
+				$this->pos->z++;
 			}
-
-			unset($this->roadCounts[$key]);
+			$this->pos->z = $this->plotBeginPos->z;
+			$this->pos->x++;
 		}
+		$this->plugin->getLogger()->debug("Plot Road Clear task completed at {$this->plot->X};{$this->plot->Z}");
 	}
 }
